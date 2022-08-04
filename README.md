cbfe838abe223352a68270106c626b4984ede289
sg-lsm <ckdgmlrt@gmail.com>
2022-08-04 10:33:22 +0900

    cartesian problem

---
+++ README.md
@@ -0,0 +1,26 @@
+# Algorithm
+
+    # 1. fivonaci
+    # 2. reserveFivonaci
+
+    # 3. factorial
+    # 4. reserveFactorial
+
+    # 5. isPrime
+    # 6. isPrimeOfBitWise
+
+    # 7. isPowerOfTwo
+
+    # 8. binarySearch
+    # 9. recursiveBinarySearch
+
+    # 10. sorting Algorithm (Bubble, Insertion, Quick, Merge)
+        # 10-1. Bubble sort
+        # 10-2 Insertion sort
+        # 10-3 Quick sort
+        # 10-4 Merge sort
+
+    # 11. problem solve
+        # 11-1 cartesian problem
+        # 11-2 climbing staircase
+        # 11-3 tower of hanoi
--- algorithm1.js
+++ algorithm1.js
@@ -279,6 +279,23 @@ function merge(leftArr, rightArr) {
   return [...soltedArr, ...leftArr, ...rightArr];
 }
 
-let arr = [8, 20, -4, 6, -2];
+// let arr = [8, 20, -4, 6, -2];
 
-console.log(mergeSort(arr));
+// console.log(mergeSort(arr));
+
+// 12. cartesian problem
+
+function cartesian(arr1, arr2) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      result.push([arr1[i], arr2[j]]);
+    }
+  }
+  return result;
+}
+
+const arr1 = [1, 2];
+const arr2 = [3, 4, 5];
+
+console.log(cartesian(arr1, arr2));
