68e5c702e9e9683926c61052f1d7271be645301c
sg-lsm <ckdgmlrt@gmail.com>
2022-08-04 13:03:15 +0900

    tower of hanoi

--- algorithm1.js
+++ algorithm1.js
@@ -336,3 +336,17 @@ function recursiveStaircase(n) {
 // console.log(recursiveStaircase(3));
 // console.log(recursiveStaircase(4));
 // console.log(recursiveStaircase(5));
+
+// 14. tower of hanoi
+
+function towerOfHanoi(n, fromRod, toRod, usingRod) {
+  if (n === 1) {
+    console.log(`move disk 1 from ${fromRod} to ${toRod}`);
+    return;
+  }
+  towerOfHanoi(n - 1, fromRod, usingRod, toRod);
+  console.log(`move disk ${n} from ${fromRod} to ${toRod}`);
+  towerOfHanoi(n - 1, usingRod, toRod, fromRod);
+}
+
+towerOfHanoi(3, "A", "C", "B");
